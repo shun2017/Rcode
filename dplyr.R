9149f92798ea28e0da866a0c9f7a6a8b7746474e
shun2017 <ps_psy@163.com>
2019-04-12 10:52:00 +0800

    the example of mine

---
+++ dplyr.R
@@ -0,0 +1,11 @@
+data<-read.csv("per.csv",header = TRUE)
+View(data)
+str(data)
+
+library(dplyr)
+
+#the function of select
+select(data, Zmyr)
+select(data, Zmyr, Zcyr,gender)
+select(data, 1:3)
+select(data, starts_with("Z"))
